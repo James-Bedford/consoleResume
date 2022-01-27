24f71889553e04a996a55311479137fb3b4c8aaa
James Bedford <jamesbedford01@hotmail.com>
2022-01-27 22:08:55 +0000

    last of 270122

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log(`Name: James Bedford\nCareer: Full Stack Engineer\\Instructor\nDescription: I like turtles
+\n\nMy Interests:\n* Fall TV (The best)\n* Locomotives\n* Observing awkward situations\n* Film soundtracks\n\n
+My Previous Experience:
+* Math, Chemistry, Physics, Biology and English tutor at Some School - Tutored students in those areas.\n
+* Undergraduate Teaching Assistant at University of Georgia - Helped grade assignments, answer questions, held lab hours,
+ and held office hours for Java and C++ Compuer Science classes.\n
+* Lead Programmer for iBeacon Experiment at Georgia Museum of Art - Created native iOS  virtual tour guide that used iBeacon
+
+`);
