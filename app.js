8772d19e4ca9b54416e948d274f86d03ccba6077
James Bedford <jamesbedford01@hotmail.com>
2022-01-28 07:36:05 +0000

    Final version complete

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+/* consoleResume Lab
+Decided to copy to example as it had a number of different formats*/
+
 console.log(`Name: James Bedford\nCareer: Full Stack Engineer\\Instructor\nDescription: I like turtles
 \n\nMy Interests:\n* Fall TV (The best)\n* Locomotives\n* Observing awkward situations\n* Film soundtracks\n\n
 My Previous Experience:
 * Math, Chemistry, Physics, Biology and English tutor at Some School - Tutored students in those areas.\n
 * Undergraduate Teaching Assistant at University of Georgia - Helped grade assignments, answer questions, held lab hours,
  and held office hours for Java and C++ Compuer Science classes.\n
-* Lead Programmer for iBeacon Experiment at Georgia Museum of Art - Created native iOS  virtual tour guide that used iBeacon
+* Lead Programmer for iBeacon Experiment at Georgia Museum of Art - Created native iOS  virtual tour guide that used iBeacon technology to triangulate position within the museum and give information about a piece.\n\n
+My Skills:\n
+* French\n
+* BAM: Trombone\n
+* Java\n
+* C / C++\n
+* BAM: Opera Singing\n
+* Python\n
+* Javascript\n
+* BAM:TypeScript\n
 
 `);
